9ab015f6071dbf19854ea0cee3e256b68b38238f
CrystalYeoh <crystalyeohje@gmail.com>
2020-08-13 10:25:33 +0800

    part 1

---
+++ notes.js
@@ -0,0 +1,8 @@
+//js .map( operator maps the dishes to   
+const menu = this.state.dishes.map((dish)=>{
+    return(
+        <div key={dish.id} className="col-12 mt-5">
+            
+        </div>
+    )
+}
